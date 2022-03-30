85cd04de30395d6e59a4eaa591d649555fccd030
Syed Faiq Ali Faisal <faiqali2010@hotmail.com>
2022-03-30 23:22:17 +0500

    Almost working counting sort

--- Lab 2/L2.py
+++ Lab 2/L2.py
@@ -11,20 +11,34 @@ Provide the pseudocode, implement the algorithm using Python, explain the codes/
 
 A: list[int] = [16, 30, 95, 51, 84, 23, 62, 44]
 
-B = [1, 5, 7, 8, 6, 4, 7, 4, 4, 6, 2]
+B: list[int] = [1, 5, 7, 8, 6, 4, 7, 4, 4, 6, 2]
 
 
 def counting_sort(array: list[int]):
-    output = [0] * len(array)
+    output = [0] * (len(array))
     count = [0] * (max(array) + 1)
 
-    for i in range(1, len(array)):
-        count[array[i]] += 1
+    for i in array:
+        count[i] += 1
 
-    for i in range(1, max(array)):
-        count[i] += count[i - 1]
+    for j in range(1, max(array)):
+        count[j] += count[j - 1]
+
+    for k in range(len(array) - 1, 0, -1):
+        output[count[array[k]] - 1] = array[k]
+        count[array[k]] -= 1
+
+    #
+    # i = len(array) - 1
+    # while i > 0:
+    #     output[count[array[i]] - 1] = array[i]
+    #     count[array[i]] -= 1
+    #     i -= 1
+
+    return output
 
 
-
-
-counting_sort(A)
+B.sort()
+print(B)
+print(counting_sort(B))
+assert counting_sort(B) == B
